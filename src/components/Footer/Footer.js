3f519e457b8208a9320ce0b2b5f1377d3b3b0dc4
Leonar Estupiñan Quiñonez <LeonarEQ@MacBook-Pro-de-Leonar.local>
2024-02-12 00:46:47 +0100

    Creación de Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+
+import "./Footer.css";
+import { Typography } from "@mui/material";
+
+import resumeData from '../../utils/resumeData'
 
 const Footer = () => {
-    return (
-        <div>
-            This is a footer component
-        </div>
-    );
+  return (
+    <div className="footer">
+      <div className="footer_left">
+              <Typography className="footer_name">{ resumeData.name}</Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="footer_copyright">
+          Designed and Developed by{" "}
+          <a href="/" target="_blank">
+            Leonar Estupiñan
+          </a>
+          <br />
+         
+          <a href="https://leonar.dev" target="_blank">
+            www.leonar.dev
+          </a>
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
